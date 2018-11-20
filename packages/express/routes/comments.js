1da97a9825ccf2b018ccccf52b70055667eb593d
Bin Xu <beesheer@gmail.com>
2018-11-20 13:22:05 -0500

    a

--- packages/express/routes/comments.js
+++ packages/express/routes/comments.js
@@ -2,17 +2,28 @@ var express = require('express');
 var router = express.Router();
 const cassandra = require('cassandra-driver');
 const config = require('../../config.json');
-const client = new cassandra.Client({ contactPoints: [config.scylla.host + ':' + config.scylla.port], keyspace: 'am_artists_test'});
+try {
+  const client = new cassandra.Client({ contactPoints: [config.scylla.host + ':' + config.scylla.port], keyspace: 'am_artists_test'});
+} catch (err) {
+
+}
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
+  let results = [];
   const query = 'SELECT * from threads';
-  const results = await client.execute(query).catch(
+  await client.eachRow(query, [], { autoPage: true, fetchSize: 1}, function (n, row) {
+    console.log(n);
+    console.log(row);
+    results.push(row);
+  });
+
+  /*const results = await client.execute(query).catch(
     (err) => {
       console.log(err);
       next(err);
     }
-  );
+  );*/
 
   res.json(results);
 });
